c41798e3b51a62520ceeb10c4cf6f562124f7219
devhasanmia <dev.hasanmia@gmail.com>
2025-07-31 13:40:45 +0600

    fix: implement comprehensive error handling including app errors, duplicate errors, and custom errors

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from "cors"
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
 
 const app = express()
 
@@ -15,5 +17,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app
