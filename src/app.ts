5b716ca9bb70e15b8e7dbd1653c6de79bb3f5f5f
devhasanmia <dev.hasanmia@gmail.com>
2025-08-25 16:31:04 +0600

    digital wallet base url update

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173", // তোমার frontend port
+    origin: "https://digital-wallet-mu.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
 }));
